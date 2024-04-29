ea228458107b3e4ee018b9b0e2c0330dd3e6bcaa
axllr8tr <axllr8tr@gmail.com>
2024-04-29 13:24:37 +0300

    [not working] move isr entry points to traps.S

--- cpu/interrupts.c
+++ cpu/interrupts.c
@@ -1,11 +1,24 @@
 #include "idt.h"
 #include "interrupts.h"
+#include "../baseio/uprint.h"
+#include "../baseio/printf.h"
 #include "../utils/panic.h"
-__attribute__((interrupt)) void interrupt_handler_generic(x86_simple_interrupt_frame_t *iframe) {
-  uprint("well");
+__attribute__((interrupt)) void interrupt_handler_generic(x86_extended_interrupt_frame_t *iframe) {
+  cprintf(0x70, 
+          "\n*** CAUGHT INTERRUPT ***\n"
+          "VECTOR %x\n"
+          "*** CAUGHT INTERRUPT ***\n",
+          iframe->vector
+  );
 } 
 
-__attribute__((interrupt)) void interrupt_handler_err(x86_simple_interrupt_frame_t *iframe) {
-  cpanic(0xfffffff, "Caught an interrupt with error code");
+__attribute__((interrupt)) void interrupt_handler_err(x86_extended_interrupt_frame_t *iframe) {
+  cprintf(0x70, 
+          "\n*** CAUGHT INTERRUPT ***\n"
+          "VECTOR %x\n"
+          "*** CAUGHT INTERRUPT ***\n",
+          iframe->vector
+  );
+  asm volatile ("hlt");
 } 
 
--- cpu/interrupts.h
+++ cpu/interrupts.h
@@ -3,31 +3,30 @@
 
 #include "../include/defs.h"
 
+// not sure if this is even right
 typedef struct iframe {
-  u16 ip;
+  u32 eip;
   u16 cs;
-  u16 flags;
-  u16 sp;
+  u32 eflags;
+  u32 esp;
   u16 ss;
-} x86_simple_interrupt_frame_t;
+} x86_simple_interrupt_frame_t; 
 
 typedef struct eiframe {
-  u32 eax;
-  u32 ebx;
-  u32 ecx;
-  u32 edx;
-  u32 esp;
-  u32 ebp;
-  u32 esi;
-  u32 edi;
-  u32 eip;
-  u32 eflags; 
-  u16 cs;
-  u16 ss;
-  u16 ds;
+  u32 di, si, bp, sp, bx, dx, cx, ax;
+  u32 ds, es, fs, gs;
+  u32 vector;
+  u32 err;
+  u32 eip, cs, eflags;
+  u32 user_sp, user_flags;
 } x86_extended_interrupt_frame_t;
 
-void interrupt_handler_generic(x86_simple_interrupt_frame_t*);
-void interrupt_handler_err(x86_simple_interrupt_frame_t*);
+// entry points
+void interrupt_handler_generic_entry(); /* traps.S */
+void interrupt_handler_err_entry(); /* traps.S */
+
+// actual handlers
+void interrupt_handler_generic(x86_extended_interrupt_frame_t*);
+void interrupt_handler_err(x86_extended_interrupt_frame_t*);
 
 #endif /* _INTERRUPTS_H */
